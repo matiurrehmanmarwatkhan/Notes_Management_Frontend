e87c476cd861568e94d963a2b136e777b8662432
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2026-06-09 22:28:06 +0500

    Change API base URL to production endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7000/api",
+  baseURL: "https://notes-management-backend-nine.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
